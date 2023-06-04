f8f93fb8f8531a62aeb8e1b63e376ed17cfef9dd
Praveen S <praveenss2806@gmail.com>
2023-06-04 20:34:23 +0530

    Revert "basic form"
    This reverts commit d3d6103831a808b5b8118b9fc7068e51a2b74a7b.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/login.component'
-import FormComponent from './components/form.component'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +13,6 @@ function App() {
 			<div class="container">
 				<Routes>
 					<Route exact path="/" element={<Login/>} />
-					<Route exact path="/form" element={<FormComponent/>} />
 					<Route path="*" element={<Login/>} />
 				</Routes>
 			</div>

d3d6103831a808b5b8118b9fc7068e51a2b74a7b
Praveen S <praveenss2806@gmail.com>
2023-06-04 19:17:06 +0530

    basic form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/login.component'
+import FormComponent from './components/form.component'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
 			<div class="container">
 				<Routes>
 					<Route exact path="/" element={<Login/>} />
+					<Route exact path="/form" element={<FormComponent/>} />
 					<Route path="*" element={<Login/>} />
 				</Routes>
 			</div>

1fcedd887d0d81955c8dc12959411257407b9096
Diyaa <diyaa2603@gmail.com>
2023-06-05 08:07:01 +0530

    changed-activity

--- frontend/src/components/activities/activity.jsx
+++ frontend/src/components/activities/activity.jsx
@@ -1,25 +1,27 @@
+import React from 'react';
 import { useNavigate } from "react-router-dom";
+import './activity.css'
 
 const Activity = (props) => {
 
-    return (
-        <div className="activity" style={{ backgroundColor: props.backgroundColor }} >
-            
-            <div className="left-div">
-                <img src={ props.activityIcon } />
-            </div>
-
-            <div className="center-div">
-                <p className="activity-title">{ props.activityTitle }</p>
-                <p className="activity-description">{ props.activityDescription }</p>
-            </div>
-
-            <div className="right-div">
-                <p className="activity-date">{ props.activityDate }</p>
-            </div>
-
+  return (
+    <div className="activity" style={{ backgroundColor: props.backgroundColor }}>
+      <div className="activity-card">
+        <div className="left-div">
+          <img src={props.activityIcon} alt="Activity Icon" />
         </div>
-    )
+        <div className="center-div">
+          <p className="activity-title">{props.activityTitle}</p>
+          <p className="activity-description">{props.activityDescription}</p>
+        </div>
+        <div className="right-div">
+          <p className="activity-date">{props.activityDate}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Activity;
+
+

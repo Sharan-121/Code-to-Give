f04222225ba9a11a07f9ea62c045bf60e6b3629d
fazil2003 <mohamedfazil463@gmail.com>
2023-06-05 21:08:51 +0530

    [F] Added the Dashboard component.

---
+++ frontend/src/components/dashboard/dashboard.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import activityIcon from "../../assets/activity_icon.png";
+import searchIcon from "../../assets/search_icon.png";
+
+const Dashboard = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>Dashboard</>
+    );
+};
+
+export default Dashboard;
--- frontend/src/components/home.component.jsx
+++ frontend/src/components/home.component.jsx
@@ -5,6 +5,7 @@ import Header from './header';
 import SideBar from './sidebar';
 import ViewActivities from './activities/view-activities.component';
 import BeneficiaryForm from './beneficiary/form.component';
+import Dashboard from './dashboard/dashboard.component';
 
 const Home = () =>{
 
@@ -17,10 +18,11 @@ const Home = () =>{
             <SideBar />
             <div className='inner-container'>
             <Routes>
-                <Route exact path="/" element={<ViewActivities />} />
+                <Route exact path="/" element={<Dashboard />} />
+                <Route exact path="/dashboard" element={<Dashboard />} />
                 <Route exact path="/activities/view" element={<ViewActivities />} />
                 <Route exact path="/beneficiary/add" element={<BeneficiaryForm />} />
-                <Route exact path="*" element={<ViewActivities />} />
+                <Route exact path="*" element={<Dashboard />} />
             </Routes>
             </div>
         </div>

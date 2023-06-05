2dc19f065857519988fdac2e76fb97bf22fc06f9
fazil2003 <mohamedfazil463@gmail.com>
2023-06-05 22:42:25 +0530

    [F] Added the ApexChart.

--- frontend/src/components/dashboard/dashboard.component.jsx
+++ frontend/src/components/dashboard/dashboard.component.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import activityIcon from "../../assets/activity_icon.png";
-import searchIcon from "../../assets/search_icon.png";
+import activityIconWhite from "../../assets/activity_icon_white.png";
+import avatarProfileWhite from "../../assets/avatar_profile_icon_white.png";
+import TopInfo from './top-info';
+import './dashboard.css';
+import ViewChart from './viewChart';
 
 const Dashboard = () => {
     const navigate = useNavigate();
 
     return (
-        <>Dashboard</>
+        <div className='dashboard-component'>
+
+            <div className='top-info-div'>
+
+                <TopInfo
+                    background="linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%)"
+                    icon = { activityIconWhite }
+                    title = "Total Activities"
+                    value = "100"
+                />
+
+                <TopInfo
+                    background="linear-gradient( 135deg, #FEC163 10%, #DE4313 100%)"
+                    icon = { activityIconWhite }
+                    title = "Total Activities"
+                    value = "100"
+                />
+
+                <TopInfo
+                    background="linear-gradient(to top, #ff0844 0%, #ffb199 100%)"
+                    icon = { activityIconWhite }
+                    title = "Total Activities"
+                    value = "100"
+                />
+
+                <TopInfo
+                    background="linear-gradient(to top, #00c6fb 0%, #005bea 100%)"
+                    icon = { activityIconWhite }
+                    title = "Total Activities"
+                    value = "100"
+                />
+
+            </div>
+            
+            <div className='main-dashboard'>
+                <ViewChart />
+            </div>
+
+        </div>
     );
 };
 

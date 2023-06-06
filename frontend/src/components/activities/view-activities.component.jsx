b2429118a055cba720c56c528b29c837d017e764
sharan-ss <sharan.s@datamyth.com>
2023-06-06 16:29:31 +0530

    rendered activity data from db

--- frontend/src/components/activities/view-activities.component.jsx
+++ frontend/src/components/activities/view-activities.component.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import Activity from './activity';
 import activityIcon from "../../assets/activity_icon.png";
 import searchIcon from "../../assets/search_icon.png";
+import defaultVariables from '../variables/variables';
 
 const ViewActivities = () => {
   const navigate = useNavigate();
+  const [activities,setActivities] = useState([]);
+ 
+
+  useEffect(() => {
+    axios.get(defaultVariables['backend-url'] + "api/v1/admin/activity").then((res) => {
+      setActivities(res.data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+  }, []);
+
+  const background = ["linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%)","linear-gradient( 135deg, #FEC163 10%, #DE4313 100%)","linear-gradient(to top, #ff0844 0%, #ffb199 100%)","linear-gradient(to top, #00c6fb 0%, #005bea 100%)","linear-gradient( 135deg, #70F570 10%, #49C628 100%)","linear-gradient(135deg, #00cc99 60%, #ccffff 100%)"
+,"linear-gradient(135deg, #ff6699 10%, #66ffcc 100%)","linear-gradient(to top, #ff0000 0%, #ff6699 100%)"]
+
+  let i = 0;
+
+
 
   return (
     <div className='view-activities'>
@@ -28,97 +49,17 @@ const ViewActivities = () => {
             <br />
 
             <div className='list-activities-container'>
-
-                <Activity
-                    background="linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Title of the Sample Activity with 2 lines"}
-                    activityDescription = 
-                    {"Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
-                    when an unknown printer took a galley of type and scrambled it to make a type \
-                    specimen book. It has survived not only five centuries, but also the leap into \
-                    electronic typesetting, remaining essentially unchanged. It was popularised in \
-                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,\
-                     and more recently with desktop publishing software like Aldus PageMaker \
-                     including versions of Lorem Ipsum."}
-
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-                <Activity
-                    background="linear-gradient( 135deg, #FEC163 10%, #DE4313 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Title of the Sample Activity with 2 lines"}
-                    activityDescription = 
-                    {"Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
-                    when an unknown printer took a galley of type and scrambled it to make a type \
-                    specimen book. It has survived not only five centuries, but also the leap into \
-                    electronic typesetting, remaining essentially unchanged. It was popularised in \
-                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,\
-                     and more recently with desktop publishing software like Aldus PageMaker \
-                     including versions of Lorem Ipsum."}
-
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-                <Activity
-                    background="linear-gradient(to top, #ff0844 0%, #ffb199 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Activity 3"}
-                    activityDescription={"Description 3"}
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-                <Activity
-                    background="linear-gradient(to top, #00c6fb 0%, #005bea 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Activity 3"}
-                    activityDescription={"Description 3"}
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-                <Activity
-                    background="linear-gradient( 135deg, #70F570 10%, #49C628 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Activity 3"}
-                    activityDescription={"Description 3"}
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-<Activity
-                    background="linear-gradient( 135deg, #70F570 10%, #49C628 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Activity 3"}
-                    activityDescription={"Description 3"}
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-<Activity
-                    background="linear-gradient( 135deg, #70F570 10%, #49C628 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Activity 3"}
-                    activityDescription={"Description 3"}
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
-<Activity
-                    background="linear-gradient( 135deg, #70F570 10%, #49C628 100%)"
-                    activityIcon={activityIcon}
-                    activityTitle={"Activity 3"}
-                    activityDescription={"Description 3"}
-                    activityDate={"04 June 2023"}
-                    activityLocation={"Bangalore"}
-                />
-
+            {activities.map(activity => (
+            <Activity
+             
+              background={background[i++]}
+              activityIcon={activityIcon}
+              activityTitle={activity.name}
+              activityDescription={activity.description}
+              activityCategory={activity.category}
+            />
+          ))}
+                
             </div>
 
         </div>

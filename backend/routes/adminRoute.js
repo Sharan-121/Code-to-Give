8007ce5db2e1e4897161bb3f1950ed3637f51512
muthuvijay5 <ymuthuvijay5@gmail.com>
2023-06-08 22:46:13 +0530

    added community metrics

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -14,33 +14,84 @@ const {
   getSessionNumber,
 } = require("../controllers/adminController");
 
-const {getActivityMetrics} = require("../controllers/activityMetricsController");
-const {genderAndCommunityWiseEngagement} = require("../controllers/genderEngagementController")
-const {ageAndCommunityWiseEngagement} = require("../controllers/ageEngagementController")
-const {communityWiseBeneficiary} = require("../controllers/communityBeneficiaryController");
-const {communityWiseAttendance} = require("../controllers/communityAttendanceController");
-const {genderDistribution} = require("../controllers/genderInfoController");
-const {employedDistribution} = require("../controllers/employedInfoController");
-const {aadharPanLink} = require("../controllers/aadharPanController");
-const {bankAccount} = require("../controllers/bankAccountController");
+const {
+  genderDistributionCommunity,
+  activeBeneficiariesCommunity,
+} = require("../controllers/communityMetrics");
+
+const {
+  getActivityMetrics,
+} = require("../controllers/activityMetricsController");
+const {
+  genderAndCommunityWiseEngagement,
+} = require("../controllers/genderEngagementController");
+const {
+  ageAndCommunityWiseEngagement,
+} = require("../controllers/ageEngagementController");
+const {
+  communityWiseBeneficiary,
+} = require("../controllers/communityBeneficiaryController");
+const {
+  communityWiseAttendance,
+} = require("../controllers/communityAttendanceController");
+const { genderDistribution } = require("../controllers/genderInfoController");
+const {
+  employedDistribution,
+} = require("../controllers/employedInfoController");
+const { aadharPanLink } = require("../controllers/aadharPanController");
+const { bankAccount } = require("../controllers/bankAccountController");
 
 router.get("/activity", validateToken, getActivities);
 router.get("/activity/:name", validateToken, getActivityByName);
 router.get("/activity/metrics/:name", validateToken, getActivityMetrics);
-router.get("/activity/metrics/cwa/:name",validateToken, communityWiseAttendance);
-router.get("/activity/metrics/cwb/:name",validateToken, communityWiseBeneficiary);
-router.get("/activity/metrics/acwe/:activityName", validateToken, ageAndCommunityWiseEngagement);
-router.get("/activity/metrics/gcwe/:activityName", validateToken, genderAndCommunityWiseEngagement);
+router.get(
+  "/activity/metrics/cwa/:name",
+  validateToken,
+  communityWiseAttendance
+);
+router.get(
+  "/activity/metrics/cwb/:name",
+  validateToken,
+  communityWiseBeneficiary
+);
+router.get(
+  "/activity/metrics/acwe/:activityName",
+  validateToken,
+  ageAndCommunityWiseEngagement
+);
+router.get(
+  "/activity/metrics/gcwe/:activityName",
+  validateToken,
+  genderAndCommunityWiseEngagement
+);
 router.post("/activity", validateToken, createActivity);
-router.get("/community/metrics/getGender/:name", validateToken, genderDistribution);
-router.get("/community/metrics/getEmployed/:name", validateToken, employedDistribution);
-router.get("/community/metrics/getAadharPan/:name", validateToken, aadharPanLink);
-router.get("/community/metrics/getBankAccount/:name", validateToken, bankAccount)
+router.get(
+  "/community/metrics/getGender/:name",
+  validateToken,
+  genderDistribution
+);
+router.get(
+  "/community/metrics/getEmployed/:name",
+  validateToken,
+  employedDistribution
+);
+router.get(
+  "/community/metrics/getAadharPan/:name",
+  validateToken,
+  aadharPanLink
+);
+router.get(
+  "/community/metrics/getBankAccount/:name",
+  validateToken,
+  bankAccount
+);
 router.get("/community/:name", validateToken, getCommunity);
 router.get("/community", validateToken, getAllCommunities);
 router.post("/community", validateToken, addCommunity);
 router.get("/session", validateToken, getAllSessions);
 router.post("/session", validateToken, createSession);
 router.get("/session/number/:name", validateToken, getSessionNumber);
+router.get("/community/metrics/gender/:name", validateToken, genderDistributionCommunity);
+router.get("/community/metrics/participation/:name", validateToken, activeBeneficiariesCommunity);
 
 module.exports = router;

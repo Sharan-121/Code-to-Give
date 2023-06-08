4324dc317d4d836972e880f3c7aec09b9fc580cc
fazil2003 <mohamedfazil463@gmail.com>
2023-06-09 00:11:37 +0530

    [F] Changed the color of community icon.

--- frontend/src/components/community/community.component.jsx
+++ frontend/src/components/community/community.component.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import TopInfo from '../dashboard/top-info';
 import { useNavigate, useParams } from "react-router-dom";
 import Community from './community';
-import communityIcon from "../../assets/community_icon.png";
+import communityIconWhite from "../../assets/community_icon_white.png";
 import searchIcon from "../../assets/search_icon.png";
 import defaultVariables from '../variables/variables';
 import "./community.css";
@@ -198,25 +198,25 @@ const CommunityDetails = () => {
             <div className='top-info-div'>
                 <TopInfo
                     background="linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%)"
-                    icon={communityIcon}
+                    icon={communityIconWhite}
                     title="Total Activities"
                     value="100"
                 />
                 <TopInfo
                     background="linear-gradient( 135deg, #FEC163 10%, #DE4313 100%)"
-                    icon={communityIcon}
+                    icon={communityIconWhite}
                     title="Total Sessions"
                     value="100"
                 />
                 <TopInfo
                     background="linear-gradient(to top, #ff0844 0%, #ffb199 100%)"
-                    icon={communityIcon}
+                    icon={communityIconWhite}
                     title="Total Beneficiaries"
                     value="100"
                 />
                 <TopInfo
                     background="linear-gradient(to top, #00c6fb 0%, #005bea 100%)"
-                    icon={communityIcon}
+                    icon={communityIconWhite}
                     title="Total Activities"
                     value="100"
                 />

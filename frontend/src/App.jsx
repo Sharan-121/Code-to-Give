02109104a11d0cd11f0e25190770f24022c11d61
Sharan-121 <sharanvignesh123@gmail.com>
2023-06-08 08:15:39 +0530

    fixed import error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/login.component'
 import Home from './components/home.component'

4c45f87ae3b112082e7f8ef22f9f36d139807b15
Sharan-121 <sharanvignesh123@gmail.com>
2023-06-09 16:38:27 +0530

    modified beneficiary metric

--- backend/controllers/communityMetricsController.js
+++ backend/controllers/communityMetricsController.js
@@ -1,8 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Community = require("../models/communityModel");
 const Session = require("../models/sessionModel");
-const Attendance = require("../models/attendanceModel");
-const Activity = require("../models/activityModel");
 const Beneficiary = require("../models/beneficiaryModel");
 
 const getCommunityMetrics = asyncHandler(async (req, res) => {
@@ -17,11 +15,8 @@ const getCommunityMetrics = asyncHandler(async (req, res) => {
     const totalActivity = await Session.distinct("activity_id", {
       community_id: community_id,
     });
-    const sessionIdArray = totalSessionID.map((session) => session._id);
 
-    const totalBeneficiaries = await Attendance.distinct("beneficiary_id", {
-      session_id: { $in: sessionIdArray },
-    });
+    const totalBeneficiaries = await Beneficiary.find({community : req.params.name});
 
     res.status(200).json({
       totalSessions: totalSessionID.length,

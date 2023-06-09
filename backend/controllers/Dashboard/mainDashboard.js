e1f51a924dba64aa3bd937f660ed4306a1a27768
muthuvijay5 <ymuthuvijay5@gmail.com>
2023-06-09 23:15:47 +0530

    added year and community

--- backend/controllers/Dashboard/mainDashboard.js
+++ backend/controllers/Dashboard/mainDashboard.js
@@ -15,7 +15,7 @@ const getDashboardMetrics = asyncHandler(async (req, res) => {
     }
     //1. No. of sessions conducted in a particular year
     if (
-      year != "None" &&
+      year !== "None" &&
       month === "None" &&
       activity === "None" &&
       community === "None"
@@ -44,39 +44,71 @@ const getDashboardMetrics = asyncHandler(async (req, res) => {
 
       const result = await Session.aggregate(pipeline);
       res.status(200).json(result);
-    };
-
-    if(year != "None" && month === "None" && activity != "None" && community == "None"){
-        const activityVal = await Activity.findOne({name : activity});
-        const pipeline = [
-            {
-                $match: {
-                    date: {
-                        $gte: new Date(year, 0, 1),
-                        $lt: new Date(year, 11, 31),
-                    },
-                    activity_id : activityVal._id
-                }
-            },
-            {
-                $group: {
-                    _id: { $month: "$date" },
-                    count: { $sum: 1 },
-                }
-            },
-            {
-                $sort: {
-                    _id: 1,
-                }
-            }
-        ];
-        const result = await Session.aggregate(pipeline);
-        res.status(200).json(result);
     }
-    
-    
-    // else if()
 
+    if (
+      year !== "None" &&
+      month === "None" &&
+      activity !== "None" &&
+      community === "None"
+    ) {
+      const activityVal = await Activity.findOne({ name: activity });
+      const pipeline = [
+        {
+          $match: {
+            date: {
+              $gte: new Date(year, 0, 1),
+              $lt: new Date(year, 11, 31),
+            },
+            activity_id: activityVal._id,
+          },
+        },
+        {
+          $group: {
+            _id: { $month: "$date" },
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $sort: {
+            _id: 1,
+          },
+        },
+      ];
+      const result = await Session.aggregate(pipeline);
+      res.status(200).json(result);
+    }
+    if (
+      year !== "None" &&
+      month === "None" &&
+      activity === "None" &&
+      community !== "None"
+    ) {
+      let result = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0,
+        11: 0,
+      };
+      const communityModel = await Community.findOne({ name: community });
+      const sessions = await Session.find({ community_id: communityModel._id });
+      for (const session of sessions) {
+        if (session.date.getFullYear() === parseInt(year)) {
+          result[session.date.getMonth()]++;
+        }
+      }
+      res.status(200).json(result);
+    }
+
+    // else if()
   }
 });
 

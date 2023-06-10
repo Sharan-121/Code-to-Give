0833806c15e22c7cf68052c4ed573f1be58c8185
fazil2003 <mohamedfazil463@gmail.com>
2023-06-10 16:18:30 +0530

    [F] Changed the design problem in the location page.

--- frontend/src/components/locations/viewLocations.jsx
+++ frontend/src/components/locations/viewLocations.jsx
@@ -91,8 +91,6 @@ const ViewLocations = () => {
 
     return (
         <>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
             <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
                 integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
                 crossorigin="" />

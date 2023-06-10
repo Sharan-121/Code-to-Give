5e182fb6eea5384562f2bbe524febb22543ebf77
fazil2003 <mohamedfazil463@gmail.com>
2023-06-11 00:25:14 +0530

    [F] Changed the version in the footer.

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="footer">
             <p className="footer-name">Email: info@tinymiracles.com</p>
             <p className="footer-name">Phone: +31 (0)20 3342686</p>
-            <p className="footer-version">Version 1.0.0</p>
+            <p className="footer-version">Version 1.0.3</p>
             <p className="footer-name">&copy; 2023 Tiny Miracles. All rights reserved.</p>
         </div>
     )

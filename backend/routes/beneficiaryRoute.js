90725d27dbcde625572c04ada4d5fae4b8fa1ee3
muthuvijay5 <ymuthuvijay5@gmail.com>
2023-06-10 11:55:13 +0530

    added update follow up status route for beneficiary

--- backend/routes/beneficiaryRoute.js
+++ backend/routes/beneficiaryRoute.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   getAvailableSessions,
   getAttendedSessions,
+  updateStatus,
 } = require("../controllers/beneficiaryController");
 const validateToken = require("../middleware/validateTokenHandler");
 
 router.get("/sessions/available", validateToken, getAvailableSessions);
 router.get("/sessions/attended", validateToken, getAttendedSessions);
+router.post("/sessions/setstatus", validateToken, updateStatus);
 
 module.exports = router;

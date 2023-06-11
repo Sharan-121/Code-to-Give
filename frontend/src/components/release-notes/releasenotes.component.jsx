2e19e7e26253c51008bcf8b4f6609358be6ebb58
fazil2003 <mohamedfazil463@gmail.com>
2023-06-12 00:06:25 +0530

    [F] Completed the release notes page.

--- frontend/src/components/release-notes/releasenotes.component.jsx
+++ frontend/src/components/release-notes/releasenotes.component.jsx
@@ -9,35 +9,35 @@ const ReleaseNotes = () => {
                 <p className="heading-small">Version 1.0.3</p>
                 <ul style={{ lineHeight: "30px"}}>
                     <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
+                    <li>Upload the csv file to add the data to the existing informations.</li>
+                    <li>The help feature lists out the most frequently asked questions.</li>
                 </ul>
             </div>
 
             <div className="form-div" style={{ textAlign: "left" }}>
                 <p className="heading-small">Version 1.0.2</p>
                 <ul style={{ lineHeight: "30px"}}>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
+                    <li>Locations of all the sessions that were conducted.</li>
+                    <li>Metrics for communities.</li>
+                    <li>Locations of all the Beneficiaries.</li>
                 </ul>
             </div>
 
             <div className="form-div" style={{ textAlign: "left" }}>
                 <p className="heading-small">Version 1.0.1</p>
                 <ul style={{ lineHeight: "30px"}}>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
+                    <li>Separate login for the staffs.</li>
+                    <li>Locations of all the communities that were covered.</li>
+                    <li>Metrics for activities.</li>
                 </ul>
             </div>
 
             <div className="form-div" style={{ textAlign: "left" }}>
                 <p className="heading-small">Version 1.0.0</p>
                 <ul style={{ lineHeight: "30px"}}>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
-                    <li>Added the option to extract the text from the image.</li>
+                    <li>Comparing option with other years.</li>
+                    <li>Main dashboard metrics based on sessions and attendances.</li>
+                    <li>Admin and staff can add the attendance for the beneficiary.</li>
                 </ul>
             </div>
 
